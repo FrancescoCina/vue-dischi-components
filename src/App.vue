42c4e060ad74dff5f444c739788f38c9d3105f32
FrancescoCina <francesco.cina04@gmail.com>
2021-09-09 16:56:59 +0200

    Creato componente Header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div id="app">
-    <div class="prova">
-      <div class="row">
-        <div class="col"></div>
-      </div>
-    </div>
+    <Header />
   </div>
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
 export default {
   name: "App",
-  components: {},
+  components: { Header },
 };
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <div class="h-100 d-flex justify-content-between align-items-center">
+      <img class="ms-4 img-fluid" src="@/assets/img/logo.png" alt="Logo" />
+      <div class="search"></div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+@import "../assets/scss/_vars.scss";
+header {
+  height: 100px;
+  background-color: $header-color;
+}
+
+img {
+  width: 50px;
+  height: 50px;
+}
+</style>
